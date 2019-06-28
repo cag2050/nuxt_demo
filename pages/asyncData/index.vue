9b21d8d29e0e460c558b12eba6b1845a20b9b404
chen_anguo <cag2050@163.com>
2019-06-28 17:12:46 +0800

    返回多个字段

--- pages/asyncData/index.vue
+++ pages/asyncData/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div>github 中，js 仓库 stars 数排名：</div>
+        total_count：{{ total_count }}
         <el-table :data="items" style="width: 70%">
             <el-table-column
                 type="index"
@@ -32,7 +33,7 @@ export default {
     name: 'Index',
     // 异步数据：https://zh.nuxtjs.org/guide/async-data
     // 1. 返回 Promise
-    /*
+    // /*
     asyncData({ params }) {
         return axios
             .get(
@@ -40,30 +41,36 @@ export default {
             )
             .then(res => {
                 return {
-                    items: res.data.items
+                    items: res.data.items,
+                    total_count: res.data.total_count
                 }
             })
     },
-    */
+    // */
     // 2. 使用 async或await
     /*
     async asyncData({ params }) {
         const { data } = await axios.get(
             `https://api.github.com/search/repositories?q=+language:js&sort=stars&order=desc`
         )
-        return { items: data.items }
+        return { items: data.items, total_count: data.total_count }
     },
     */
     // 3. 使用回调函数
+    /*
     asyncData({ params }, callback) {
         axios
             .get(
                 `https://api.github.com/search/repositories?q=+language:js&sort=stars&order=desc`
             )
             .then(res => {
-                callback(null, { items: res.data.items })
+                callback(null, {
+                    items: res.data.items,
+                    total_count: res.data.total_count
+                })
             })
     },
+    */
     methods: {
         indexMethod(index) {
             return index + 1
