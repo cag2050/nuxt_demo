fb1a5a8db75779f824e742d02590f217bf536b61
chen_anguo <cag2050@163.com>
2019-06-28 16:36:31 +0800

    获取异步数据的3种方式

--- pages/asyncData/index.vue
+++ pages/asyncData/index.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
-        {{ total_count }}
+        <div>github 中，js 仓库 stars 数排名：</div>
+        <el-table :data="items" style="width: 70%">
+            <el-table-column
+                type="index"
+                :index="indexMethod"
+            ></el-table-column>
+            <el-table-column prop="full_name" label="项目全名" width="300px">
+            </el-table-column>
+            <el-table-column
+                prop="stargazers_count"
+                label="stars 数"
+                width="100px"
+            >
+            </el-table-column>
+            <el-table-column label="仓库地址">
+                <template slot-scope="scope">
+                    <a :href="scope.row.html_url" target="_blank">{{
+                        scope.row.html_url
+                    }}</a>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -9,16 +30,44 @@ import axios from 'axios'
 
 export default {
     name: 'Index',
-    asyncData() {
+    // 异步数据：https://zh.nuxtjs.org/guide/async-data
+    // 1. 返回 Promise
+    /*
+    asyncData({ params }) {
         return axios
             .get(
                 `https://api.github.com/search/repositories?q=+language:js&sort=stars&order=desc`
             )
             .then(res => {
                 return {
-                    total_count: res.data.total_count
+                    items: res.data.items
                 }
             })
+    },
+    */
+    // 2. 使用 async或await
+    /*
+    async asyncData({ params }) {
+        const { data } = await axios.get(
+            `https://api.github.com/search/repositories?q=+language:js&sort=stars&order=desc`
+        )
+        return { items: data.items }
+    },
+    */
+    // 3. 使用回调函数
+    asyncData({ params }, callback) {
+        axios
+            .get(
+                `https://api.github.com/search/repositories?q=+language:js&sort=stars&order=desc`
+            )
+            .then(res => {
+                callback(null, { items: res.data.items })
+            })
+    },
+    methods: {
+        indexMethod(index) {
+            return index + 1
+        }
     }
 }
 </script>
